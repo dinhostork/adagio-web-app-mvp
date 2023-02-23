718da33f84ac9673c003d147d4ecccb04d569375
Dinho Stork <dinhostork@live.com>
2023-02-23 18:17:12 -0300

    feat: theming

---
+++ src/services/theme.service.ts
@@ -0,0 +1,18 @@
+import { defaultTheme, Theme } from "@/app/themes";
+
+export function setTheme(theme: Theme): void {
+    localStorage.setItem("adagio-theme", JSON.stringify(theme))
+}
+
+export function getTheme(): Theme {
+    const themeString = localStorage.getItem('adagio-theme')
+
+    if(!themeString){
+        setTheme(defaultTheme)
+        return defaultTheme
+    }
+
+    const theme: Theme = JSON.parse(themeString)
+    return theme
+    
+}
