d199f0819afef466e2a596945db00584737e13a1
Dinho Stork <dinhostork@live.com>
2023-02-24 12:33:03 -0300

    fix: unrecognized line tag

--- src/app/dashboard/resources/components/divider.ts
+++ src/app/dashboard/resources/components/divider.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Divider = styled.line`
+export const Divider = styled.div`
     height: 0.01px;
     width: 100%;
     border-top: solid 1px ${props => props.theme.colors.enffort};
